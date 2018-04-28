a8539eab51f5df81e32afb955986909991fe1097
李武刚 <wugang@yihe-inc.com>
2018-04-28 15:14:58 +0800

    添加写入mysql数据库的逻辑

--- book_sacrpy/book_sacrpy/pipelines.py
+++ book_sacrpy/book_sacrpy/pipelines.py
@@ -5,7 +5,20 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pymysql
+from book_sacrpy.items import BookItem
 
-class BookSacrpyPipeline(object):
+class BookIntodbPipeline(object):
+
+    def __init__(self):
+        self.conn = pymysql.connect("localhost","root","root","book",charset="utf8")
+        self.cursor = self.conn.cursor()
+
+
     def process_item(self, item, spider):
-        return item
+        insert_sql = '''
+            insert into book(title,isbn,price) VALUES ('{}','{}','{}')
+        '''
+        self.cursor.execute(insert_sql.format(item['title'],item['isbn'],item['price']))
+        self.conn.commit()
+        # return item
